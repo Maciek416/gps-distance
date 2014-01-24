25f482e8acc92682ec88104c0c9744cd6ca9e798
Maciej Adwent <maciekadwent@gmail.com>
2014-01-23 20:13:52 -0800

    Measure distance along paths

--- README.md
+++ README.md
@@ -12,8 +12,28 @@ Example
 
 var distance = require('gps-distance');
 
-var result = distance(45.527517, -122.718766, 45.373373, -121.693604)
+
+// Between two points:
+
+var result = distance(45.527517, -122.718766, 45.373373, -121.693604);
 
 // result is 81.78450202539503
 
+
+// Along a path:
+var path = [
+  [45.527517, -122.718766],
+  [45.373373, -121.693604],
+  [45.527517, -122.718766]
+];
+
+var result2 = distance(path);
+
+// result2 is 163.56900405079006
+
 ```
+
+Notes
+-----
+
+Distances are returned in kilometers and computed using the Haversine formula.
--- test/test.js
+++ test/test.js
@@ -9,10 +9,35 @@ describe('gps-distance', function() {
 
   describe('distance', function() {
 
-    it('should measure distances correctly', function(done) {
+    it('should measure a distance correctly', function(done) {
 
       var libResult = distance(45.527517, -122.718766, 45.373373, -121.693604)
       libResult.should.be.approximately(81.784, 0.1);
+      done();
+      
+    });
+
+    it('should measure the distance of a short path correctly', function(done) {
+
+      var path = [
+        [45.527517, -122.718766],
+        [45.373373, -121.693604]
+      ];
+      var libResult = distance(path);
+      libResult.should.be.approximately(81.784, 0.1);
+      done();
+      
+    });
+
+    it('should measure the distance of a longer path correctly', function(done) {
+
+      var path = [
+        [45.527517, -122.718766],
+        [45.373373, -121.693604],
+        [45.527517, -122.718766]
+      ];
+      var libResult = distance(path);
+      libResult.should.be.approximately(81.784 * 2, 0.1);
       done();
       
     });
