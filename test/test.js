b0fcd2a636cac0c455811c6ccb2c55dfc770514b
Maciej Adwent <maciekadwent@gmail.com>
2014-01-26 16:29:34 -0800

    added more tests

--- test/test.js
+++ test/test.js
@@ -4,6 +4,8 @@ process.env.NODE_ENV = 'test';
 
 var should = require('should');
 var distance = require('../lib/distance');
+var gpx = require('gpx-stream');
+var fs = require('fs');
 
 describe('gps-distance', function() {
 
@@ -31,14 +33,25 @@ describe('gps-distance', function() {
 
     it('should measure the distance of a longer path correctly', function(done) {
 
-      var path = [
-        [45.527517, -122.718766],
-        [45.373373, -121.693604],
-        [45.527517, -122.718766]
-      ];
-      var libResult = distance(path);
-      libResult.should.be.approximately(81.784 * 2, 0.1);
-      done();
+      var points = new gpx();
+      var source = fs.createReadStream('./test/oregon.gpx');
+      var path = [];
+
+      source.pipe(points);
+
+      points.on('readable', function(){
+        var point;
+
+        while(point = points.read()){
+          path.push([point.lat,point.lon]);
+        }
+      });
+
+      points.on('end', function(){
+        distance(path).should.be.approximately(7.7, 0.1);
+        done();
+      });
+
       
     });
 
